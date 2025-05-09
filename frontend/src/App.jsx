bc0835b5089169317cacea1d1dce57d6826ef128
Ramasubramanya-MS <ramasubramanyams1729@gmail.com>
2025-05-09 12:45:10 +0530

    Add Routing and Favorites Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
+import {Routes, Route} from "react-router-dom"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard from './components/moviecard'
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 function App() {
   return (
     // Return only one parent element
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar/>
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/favorites" element = {<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+
   )
 }
 
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No Favoritye Movies Yet</h2>
+        <p>Start addiong movies to your favorites and they will appear here</p>
+    </div>
+}
+
+export default Favorites
