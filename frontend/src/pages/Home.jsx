9060c2d499eefd70f3c421ee12317e637ca5fd15
Ramasubramanya-MS <ramasubramanyams1729@gmail.com>
2025-05-09 11:16:00 +0530

    Add State

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import MovieCard from "../components/moviecard"
+import { useState } from "react";
 
 function Home() {
-
+    const [searchQuery, setSearchQuery] = useState("");
+    // When there is a change in state you can rerender the components
     const movies = [
         {id: 1, title: "John Wick", release_date: "2020"},
         {id: 1, title: "Terminator 1", release_date: "1999"},
@@ -10,8 +12,10 @@ function Home() {
 
     ]
 
-    const onSearch = () => {
-
+    const onSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("");
     }
 
     return (
@@ -19,13 +23,17 @@ function Home() {
             <form onSubmit={onSearch} className="search-form">
                 <input type="text" 
                 placeholder="Search for Movies..." 
-                className="search-input"/>
+                className="search-input"
+                value = {searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
 
                 <button type="submit" className="search-button">Search</button>
             </form>
             <div className="movies-grid">
-                {movies.map((movie) => (
-                    <MovieCard movie={movie} key={movie.id} />
+                {movies.map(
+                    (movie) => 
+                    movie.title.toLowerCase().startsWith(searchQuery) && (<MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
         </div>
